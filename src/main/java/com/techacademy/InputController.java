cf024a60d70f869407382660d03b6b184c25e5e6
DomesticSnail <philliptowler94@gmail.com>
2024-12-06 17:44:53 +0900

    2回目のコミット

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,14 +1,18 @@
 package com.techacademy;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class InputController {
 
     @GetMapping("/input")
-    public String getInput() {
+    public String getInput(@RequestParam(value = "previous", required = false) String previous, Model model) {
+        // Add the previous value to the model if it's not null
+        model.addAttribute("previous", previous);
         return "input";
     }
 
